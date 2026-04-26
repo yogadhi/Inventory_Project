ffe0d0395e45b1c67e5a780518649e7c76c88ce4
Yoga Adhi <yogadhiprananda@gmail.com>
2026-04-26 16:08:02 +0700

    change init db schema

--- init-db/01_schema.sql
+++ init-db/01_schema.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `inventory_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `inventory_db`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: inventory_db
